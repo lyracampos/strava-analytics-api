a15367d228b1504fa578de40de3bdc0ed8c22155
Frederico Leão <fredericoleaob@gmail.com>
2024-04-23 22:28:12 -0300

    Get Activities

--- controller/activities.go
+++ controller/activities.go
@@ -4,14 +4,50 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/lyracampos/strava-analytics-api/entities"
 )
 
 func Activities(w http.ResponseWriter, r *http.Request) {
-	activities := entities.List()
-	err := json.NewEncoder(w).Encode(activities)
+	var after time.Time
+	var err error
+	if r.URL.Query().Get("start") != "" {
+		after, err = time.Parse("2006-01-02", r.URL.Query().Get("start"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err = w.Write([]byte("data inicial inválida"))
+			if err != nil {
+				log.Printf("erro ao mostrar o erro: %v", err)
+			}
+			return
+		}
+	}
+	var before time.Time
+	if r.URL.Query().Get("end") != "" {
+		before, err = time.Parse("2006-01-02", r.URL.Query().Get("end"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err = w.Write([]byte("data final inválida"))
+			if err != nil {
+				log.Printf("erro ao mostrar o erro: %v", err)
+			}
+			return
+		}
+	}
+	listParams := entities.ListParams{
+		Page:    1,
+		PerPage: 100,
+		After:   after,
+		Before:  before,
+	}
+	activities, err := entities.List(listParams)
+	if err != nil {
+		log.Printf("erro ao listar atividades")
+	}
+	err = json.NewEncoder(w).Encode(activities)
 	if err != nil {
 		log.Printf("erro ao retornar dados")
 	}
+
 }
--- entities/activity.go
+++ entities/activity.go
@@ -1,6 +1,12 @@
 package entities
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
 
 type Activity struct {
 	Id                 int64
@@ -26,19 +32,103 @@ type Activity struct {
 	AverageWatts       float32
 }
 
-func List() []Activity {
-	return []Activity{
-		{
-			Id:       288,
-			UserId:   23946923746,
-			Title:    "foo ride",
-			MaxSpeed: 220.88,
-		},
-		{
-			Id:       282,
-			UserId:   23946923742,
-			Title:    "smart ride",
-			MaxSpeed: 320.88,
-		},
+type StravaActivity struct {
+	Id                 int64
+	UserId             int64 `json:"user_id"`
+	Name               string
+	Distance           float64
+	MovingTime         float64 `json:"moving_time"`
+	ElapsedTime        float64 `json:"elapsed_time"`
+	TotalElevationGain float64 `json:"total_elevation_gain"`
+	Type               string
+	SportType          string    `json:"sport_type"`
+	WorkoutType        int32     `json:"workout_type"`
+	StartDate          time.Time `json:"start_date"`
+	UtcOffset          float64   `json:"utc_offset"`
+	AchievementCount   int32     `json:"achievement_count"`
+	KudosCount         int32     `json:"kudos_count"`
+	Trainer            bool
+	Commute            bool
+	GearId             string `json:"gear_id"`
+	Manual             bool
+	AverageSpeed       float32 `json:"average_speed"`
+	MaxSpeed           float32 `json:"max_speed"`
+	AverageWatts       float32 `json:"average_watts"`
+}
+
+type ListParams struct {
+	Before  time.Time
+	After   time.Time
+	Page    int
+	PerPage int
+}
+
+func List(listParams ListParams) ([]*Activity, error) {
+	stravaUrl := fmt.Sprintf(
+		"https://www.strava.com/api/v3/athlete/activities?after=%d&before=%d",
+		listParams.After.Unix(),
+		listParams.Before.Unix(),
+	)
+	req, err := http.NewRequest(http.MethodGet, stravaUrl, nil)
+	if err != nil {
+		return []*Activity{}, fmt.Errorf("erro ao tentar criar Request: %w", err)
 	}
+
+	req.Header.Add(
+		"Authorization",
+		"Bearer ee16a0f42fdcbb0c51f4c57ec6ef5f1f3d329f8d",
+	)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return []*Activity{}, fmt.Errorf("erro ao fazer o Request: %w", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		return []*Activity{}, fmt.Errorf("erro ao fazer o Request: %w", err)
+	}
+
+	resJson, err := io.ReadAll(res.Body)
+	if err != nil {
+		return []*Activity{}, fmt.Errorf("erro ao carregar o response: %w", err)
+	}
+
+	var stravaActivitiesList []*StravaActivity
+	err = json.Unmarshal(resJson, &stravaActivitiesList)
+	if err != nil {
+		return []*Activity{}, fmt.Errorf("erro ao interpretar o response: %w", err)
+	}
+	activitiesList := activitiesConverter(stravaActivitiesList)
+	return activitiesList, nil
+}
+
+func activitiesConverter(stravaActivitiesList []*StravaActivity) []*Activity {
+	activitiesList := make([]*Activity, 0, len(stravaActivitiesList))
+	for _, item := range stravaActivitiesList {
+		activity := Activity{
+			Id:                 item.Id,
+			UserId:             item.UserId,
+			Title:              item.Name,
+			Distance:           item.Distance,
+			MovingTime:         item.MovingTime,
+			ElapsedTime:        item.ElapsedTime,
+			TotalElevationGain: item.TotalElevationGain,
+			Type:               item.Type,
+			SportType:          item.SportType,
+			WorkoutType:        item.WorkoutType,
+			StartDate:          item.StartDate,
+			UtcOffset:          item.UtcOffset,
+			AchievementCount:   item.AchievementCount,
+			KudosCount:         item.KudosCount,
+			Trainer:            item.Trainer,
+			Commute:            item.Commute,
+			GearId:             item.GearId,
+			Manual:             item.Manual,
+			AverageSpeed:       item.AverageSpeed,
+			MaxSpeed:           item.MaxSpeed,
+			AverageWatts:       item.AverageWatts,
+		}
+		activitiesList = append(activitiesList, &activity)
+	}
+	return activitiesList
 }
